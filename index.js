271907e3ef1a6ad27c6a52fb3517dcc38ab044eb
JOAQUÍN IRIBAS <49627045@ort>
2026-05-15 11:43:20 -0300

    Conexion a SupaBase

--- index.js
+++ index.js
@@ -1,49 +1,26 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const https = require('https');
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY 
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-app.get('/', async (req, res) => {
-  const { mes, anio, tipos_delito, barrio } = req.query;
+// app.get('/usuarios', async (req, res) => {
+//   const { data, error } = await supabase.from('Usuarios').select('nombre, apellido');
+//   if (error) return res.status(400).json(error);
+//   res.json(data);
+// })
 
-  let query = supabase.from('delitos').select('*');
-
-  function pad(n) {
-    return String(n).padStart(2, '0');
-  }
-
-  if (mes && anio) {
-    const m = pad(mes);
-    const start = `${anio}-${m}-01`;
-    const end = new Date(anio, Number(m), 0).toISOString().slice(0, 10);
-    query = query.gte('fecha', start).lte('fecha', end);
-  } else if (anio) {
-    const start = `${anio}-01-01`;
-    const end = `${anio}-12-31`;
-    query = query.gte('fecha', start).lte('fecha', end);
-  } else if (mes) {
-    const m = pad(mes);
-    query = query.ilike('fecha', `%-${m}-%`);
-  }
-
-  if (barrio) query = query.ilike('barrio', `%${barrio}%`);
-
-  if (tipos_delito) {
-    const listaDelitos = tipos_delito.split(',');
-    query = query.in('tipo_delito', listaDelitos);
-  }
-
-  const { data, error } = await query;
-
-  if (error) return res.status(400).json(error);
-  res.json(data);
+app.get('/', (req, res) => {
+  res.send('<button>Subir</button>');
 });
 
 app.listen(PORT, () => {
